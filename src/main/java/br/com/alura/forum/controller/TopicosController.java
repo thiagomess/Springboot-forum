cc1fe4eae7505ed910c23f9bde7a8f0d5ee3d43e
Thiago Gomes <thiagogomes19@hotmail.com>
2019-08-18 15:06:08 -0300

    4. Trabalhando com POST

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,24 +1,31 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-//    Passando na url o paramentro /topicos?nomeCurso=Spring Boot
-    @RequestMapping(value = "/topicos", method = RequestMethod.GET)
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    //    Passando na url o paramentro /topicos?nomeCurso=Spring Boot
+    @GetMapping
     public List<TopicoDto> lista(String nomeCurso) {
         final List<Topico> topicos;
         if (nomeCurso == null) {
@@ -28,4 +35,17 @@ public class TopicosController {
         }
         return TopicoDto.converter(topicos);
     }
+
+    @PostMapping
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
+
+        Topico topico = topicoForm.converter(cursoRepository);
+        topicoRepository.save(topico);
+
+        //Cria a URI
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        //Devolve o status 201, a URI e no body, devolve o objeto criado
+        return ResponseEntity.created(uri).body(new TopicoDto(topico));
+    }
+
 }
