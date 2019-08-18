602d94315f8ae58fc3296a47bba3f4b5102fd0c6
Thiago Gomes <thiagogomes19@hotmail.com>
2019-08-18 13:09:08 -0300

    3. Usando Spring Data

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,21 +1,31 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicosController {
 
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+//    Passando na url o paramentro /topicos?nomeCurso=Spring Boot
     @RequestMapping(value = "/topicos", method = RequestMethod.GET)
-    public List<TopicoDto> lista(){
-        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-        return TopicoDto.converter(Arrays.asList(topico, topico));
+    public List<TopicoDto> lista(String nomeCurso) {
+        final List<Topico> topicos;
+        if (nomeCurso == null) {
+            topicos = topicoRepository.findAll();
+        } else {
+            topicos = topicoRepository.carregarCursoPeloNome(nomeCurso);
+        }
+        return TopicoDto.converter(topicos);
     }
 }
---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,24 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+/* Um pouco mais sobre o poder do JpaRepository
+* https://edermfl.wordpress.com/2017/04/27/aprendendo-spring-5-jparepository/
+* https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.entity-graph
+*
+*/
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+    //Usando o padrão do Spring FindBy? ele cria a propria query, no caso de relacionamento, primeiro se da o nome
+    // do relacionamento concaternado com o nome do atributo ou usando o "_" para separar
+    List<Topico> findByCursoNome(String nomeCurso);
+
+    //Caso nao queira usar o padrão do findBy, pode se usar o @Query e escrever a query
+    @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+    List<Topico> carregarCursoPeloNome(@Param("nomeCurso") String nomeCurso);
+}
