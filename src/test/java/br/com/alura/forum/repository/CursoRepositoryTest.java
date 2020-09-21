2a5cde2f75ef33f63037e12b9a62b3c19156355b
Thiago <thiagogomes19@hotmail.com>
2020-09-21 19:17:50 -0300

    3.3 Testes

---
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,47 @@
+package br.com.alura.forum.repository;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.alura.forum.modelo.Curso;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ActiveProfiles("test")
+public class CursoRepositoryTest {
+	
+	@Autowired
+	private CursoRepository cursoRepository;
+	
+	@Autowired
+	private TestEntityManager em;
+
+	@Test
+	public void deveriaCarregarUmCursoAoBuscarPeloNome() {
+		String nomeCurso = "HTML 5";
+		
+		Curso newCurso = new Curso(nomeCurso, "Programação");
+		em.persist(newCurso);
+		
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		
+		Assert.assertNotNull(curso);
+		Assert.assertEquals(nomeCurso, curso.getNome());
+	}
+	
+	@Test
+	public void naoDeveriaCarregarUmCursoAoBuscarPeloNome() {
+		String nomeCurso = "JPA";
+		
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		
+		Assert.assertNull(curso);
+	}
+
+}
